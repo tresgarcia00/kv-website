8eda69a12c6edcd34ae0c7579b4e0cc091e908d5
tresgarcia00 <tresgarcia00@gmail.com>
2022-03-11 14:20:32 -0500

    first upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA28RQDtu9A1v1bywa7tesAU9EgFczg8-8",
+  authDomain: "kiyoteventure.firebaseapp.com",
+  databaseURL: "https://kiyoteventure.firebaseio.com",
+  projectId: "kiyoteventure",
+  storageBucket: "kiyoteventure.appspot.com",
+  messagingSenderId: "96744855163",
+  appId: "1:96744855163:web:19e03643051b3a8735b4de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
